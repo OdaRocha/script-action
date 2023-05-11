6ab4aa896a3f8b6dd0251ebea63e8ba1feda919b
Odair Rocha <odairfernando95@gmail.com>
2023-05-11 14:01:20 -0300

    removendo imports

--- src/main.py
+++ src/main.py
@@ -1,15 +1,5 @@
-import json
-import hcl
-
 from consumer_service import map_to_consumer
 from throttling_validator import validate_duplicate
-
-
-
-def tfvar_to_class(terraform_file):
-    with open(terraform_file, 'r') as fp:
-        obj = hcl.load(fp)
-    return obj
 
 def start():
     filePath = '../consumers.json'
