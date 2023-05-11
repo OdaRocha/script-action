79124bdfeab609f2ffaea369c319935831eaff9c
Odair Rocha <odairfernando95@gmail.com>
2023-05-11 14:02:45 -0300

    alterando arquivo de local apra testes

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from consumer_service import map_to_consumer
 from throttling_validator import validate_duplicate
 
 def start():
-    filePath = '../consumers.json'
+    filePath = './consumers.json'
     consumerList = map_to_consumer(filePath)
     validate_duplicate(consumerList[0])
 
